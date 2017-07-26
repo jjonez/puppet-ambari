8ab153c0aeef393fb8360c37e6ef22728637db44
Jim Jones <welikejeeps@gmail.com>
2017-07-26 01:51:59 -0400

    Added JDK instead of JRE. Required by Ambari. Also parameterized java_home

--- manifests/os/jdk.pp
+++ manifests/os/jdk.pp
@@ -2,7 +2,7 @@
 class ambari::os::jdk inherits ambari {
 
  # Install JDK
- package { 'java-1.8.0-openjdk':
+ package { 'java-1.8.0-openjdk-devel':
    ensure => 'installed',
    require => Package['yum']
  }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class ambari::params {
   $amb_sever_hostname = 'puppet.local'
-  $java_home = '/usr/lib/jvm/java-1.8.0-openjdk'
+  $java_home = '/usr/lib/jvm/java-openjdk'
 
   # Blueprint params 
   $num_datanodes = 1 
