734f2c1175a70722cc7386f778f165e1e3529482
Nasheb Ismaily <nasheb@gmail.com>
2017-07-26 08:39:00 -0500

    removed expect code

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -2,26 +2,10 @@ class ambari::server::config() {
 
  $java_home = $::ambari::server::java_home
 
- file { 
-   'ambari_setup_script' :
-   ensure => 'file',
-   source => 'puppet:///modules/ambari/server/ambari_setup_script.exp',
-   path => '/tmp/ambari_setup_script.exp',
-   owner => 'root',
-   group => 'root',
-   mode  => '0777',
-   require => Class['::ambari::server::install'],
-   notify => Exec['run_ambari_server_setup_script']
- } ->
-# Incase Expect is giving us trouble
-# exec {
-#  'run_ambari_server_setup_script':
-#  command => "/tmp/ambari_setup_script.exp ${java_home}"
-# }
- # This will run accepting all default values
  exec {
-  'run_ambari_server_setup_script':
-  command => "/usr/sbin/ambari-server setup -j ${java_home} -s"
+   'run_ambari_server_setup_script':
+   command => "/usr/sbin/ambari-server setup -j ${java_home} -s",
+   require => Class['::ambari::server::install']
  }
 
 }
